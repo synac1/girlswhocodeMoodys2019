31db901ea5c81097926391e06451dd92a06262b8
Yanilda <synac1@users.noreply.github.com>
2019-07-03 10:10:38 -0700

    Changing the folder

---
+++ docs/myScript.js
@@ -0,0 +1,12 @@
+
+function add (a, b){
+  var sum = a+b;
+  return sum;
+}
+function whatsMyName(){
+  alert("My name is Yani!");
+  var name = document.getElementById('wname');
+  name.innerHTML = " My name is Yani!";
+  name.style.color="purple";
+  name.style.fontSize= "78px";
+}
